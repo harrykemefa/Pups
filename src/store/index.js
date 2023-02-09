a619b8e457866e13a5782930c1e3c91591156a9d
Harry Kemefa <harrykemefa@gmail.com>
2023-02-09 03:54:13 +0100

    basic vuex store setup

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+
+
+const store = createStore({
+  state () {
+    return {
+      all: [],
+    }
+  },
+  mutations: {
+    // Mutations goes here
+  },
+
+  actions: {
+     // Actions goes here
+    },
+
+})
